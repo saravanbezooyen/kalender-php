7e5f99bbb5e1f3ff3d4833046e2f921d4852cd61
Sara van Bezooyen <99021834@mydavinci.nl>
2017-04-18 10:11:47 +0200

    update styling

--- view/birthday/index.php
+++ view/birthday/index.php
@@ -9,30 +9,22 @@
 	$birthdays = $stmt->fetchALL();
 	?>
 
-<html>
-	<body>
-		<main>
-
-
+<div class="container">
 		        <?php foreach($birthdays as $birthday){ ?>
 
 		        <?php	 	
 			        if  ($currentMonth != $text[$birthday["month"]-1]){
 			        	$currentMonth = $text[$birthday["month"]-1];
-			        	echo '<h1>';echo $text[$birthday["month"]-1]; echo '</h1>';
+			        	echo '<h1>';echo $text[$birthday["month"]-1]; echo '</h1>';?>
+			        	<h2><?php echo $birthday["day"]; ?></h2>
+						<a href="edit/<?php echo $birthday["id"]; ?>"><?php echo $birthday["person"] ?></a>
+		  				<a href="delete/<?php echo $birthday["id"]; ?>"> x </a>
+		  			<p>(<?php echo $birthday["year"]; ?>)</p>
+			        	<?php
 			        }
 		       	?>
 
-		        	<h2><?php echo $birthday["day"]; ?> </h2>
-		  		<p>
-		  			<a href="edit.php?id=<?php echo $birthday["id"]; ?>"><?php echo $birthday["person"] ?></a>  <a href="<?= URL ?>birthday/delete/<?= $birthday['birthday_id'] ?>>x</a>
-		  			<p>(<?php echo $birthday["year"]; ?>)</p>
-
-		  			
-		  		</p>
 				<?php } ?>
 
-				<p><a href="create.php">+ toevoegen</a></p>
-		</main>
-	</body>
-</html>
+				<p><a href="create">+ toevoegen</a></p>
+</div>
